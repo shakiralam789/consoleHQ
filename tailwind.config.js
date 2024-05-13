8a910caa58ce558b97c741317bd729764bd74ffa
shakirr1176 <shakir@appolay.com>
2024-05-14 02:50:42 +0600

    added new contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
         'secondary': '#FF6C3E',
         'default-green': '#27AE60',
         'gray-dark': '#082020',
-        'gray-light': '#64676D'
+        'gray-light': '#64676D',
+        'gray-medium' : '#D5E0E0',
+         'blue2' : '#2D9CDB'
       }
     },
   },
